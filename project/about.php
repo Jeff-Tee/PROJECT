d5a45273b917f4790a174794fee30069c15c0475
Jeff-Tee <39340369+Jeff-Tee@users.noreply.github.com>
2020-04-02 09:33:21 +0000

    INITIAL COMMIT

---
+++ project/about.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TechKlinic</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+    <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css"  href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="js/modernizr.custom.js"></script>
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+  <nav id="menu" class="navbar navbar-default navbar-fixed-top">
+    <div class="container"> 
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        <a class="navbar-brand page-scroll" href="index.php"><i class="fa fa-moon-o fa-rotate-90"></i> TechKlinic</a> </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="index.php" class="page-scroll">Home</a></li>
+          <li><a href="service.php" class="page-scroll">Services</a></li>
+          <li><a href="#page-top" class="page-scroll">About</a></li>
+          <li><a href="contact.php" class="page-scroll">Contact</a></li>
+        </ul>
+      </div> 
+    </div>
+  </nav>
+<header id="header">
+  <div class="intro text-center">
+    <div class="overlay">
+      <div class="container">
+        <div class="row">
+          <div class="intro-text">
+            <h1>Welcome to <span class="brand">TechKlinic</span></h1>
+            <p>WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+            <a href="service.php" class="btn btn-default btn-lg page-scroll">Learn More</a> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<div id="about">
+  <div class="container">
+    <div class="col-md-8 col-md-offset-2 section-title text-center">
+      <h2>About Us</h2>
+      <p>WE ARE STUDENTS THAT LOVES WHAT WE DO. WE ARE STUDENTS THAT LOVES WHAT WE DO. WE ARE STUDENTS THAT LOVES WHAT WE DO. WE ARE STUDENTS THAT LOVES WHAT WE DO. WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-md-6"> <img src="img/IMG_0339.jpg" class="img-responsive" alt=""> </div>
+      <div class="col-xs-12 col-md-6">
+        <div class="about-text">
+          <h4>Who We Are</h4>
+          <p>WE ARE STUDENTS THAT LOVES WHAT WE DO. WE ARE STUDENTS THAT LOVES WHAT WE DO. WE ARE STUDENTS THAT LOVES WHAT WE DO. WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+          <h4>What We Do</h4>
+          <p>WE ARE STUDENTS THAT LOVES WHAT WE DO. WE ARE STUDENTS THAT LOVES WHAT WE DO. WE ARE STUDENTS THAT LOVES WHAT WE DO. WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+          <h4>Why Choose Us</h4>
+          <p>WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="contact" class="text-center">
+  <div class="overlay">
+        <div class="social">
+          <ul>
+            <li><a href="https://web.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="container text-center">
+    <div class="fnav">
+      <p>Copyright &copy; Jeffery_Ruth</p>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.js"></script> 
+<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script> 
+<script type="text/javascript" src="js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="js/contact_me.js"></script> 
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
---
+++ project/service.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TechKlinic</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+    <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css"  href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="js/modernizr.custom.js"></script>
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+  <div class="container"> 
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand page-scroll" href="index.php"><i class="fa fa-moon-o fa-rotate-90"></i> TechKlinic</a> </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php" class="page-scroll">Home</a></li>
+        <li><a href="#page-top" class="page-scroll">Services</a></li>
+        <li><a href="about.php" class="page-scroll">About</a></li>
+        <li><a href="contact.php" class="page-scroll">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div id="services" class="text-center">
+  <div class="container">
+    <div class="col-md-8 col-md-offset-2 section-title">
+      <h2>Our Services</h2>
+      <p>WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+    </div>
+    <div class="row">
+      <div class="col-xs-6 col-md-3"> <i class="fa fa-desktop"></i>
+        <h4>Web Design</h4>
+        <p>WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+      </div>
+      <div class="col-xs-6 col-md-3"> <i class="fa fa-gears"></i>
+        <h4>App Development</h4>
+        <p>WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+      </div>
+      <div class="col-xs-6 col-md-3"> <i class="fa fa-rocket"></i>
+        <h4>Branding</h4>
+        <p>WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+      </div>
+      <div class="col-xs-6 col-md-3"> <i class="fa fa-line-chart"></i>
+        <h4>Marketing</h4>
+        <p>WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="portfolio">
+  <div class="container">
+    <div class="col-md-8 col-md-offset-2 section-title text-center">
+      <h2>Our Portfolio</h2>
+      <p>WE ARE STUDENTS THAT LOVES WHAT WE DO</p>
+    </div>
+    <div class="categories">
+      <ul class="cat">
+        <li>
+          <ol class="type">
+            <li><a href="#" data-filter="*" class="active">All</a></li>
+            <li><a href="#" data-filter=".web">Web Design</a></li>
+            <li><a href="#" data-filter=".app">App Development</a></li>
+            <li><a href="#" data-filter=".branding">Branding</a></li>
+          </ol>
+        </li>
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/IMG_0328.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <small>Web Design</small> </div>
+              <img src="img/portfolio/IMG_0328.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 app">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/IMG_0330.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <small>App Development</small> </div>
+              <img src="img/portfolio/IMG_0330.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/IMG_0335.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <small>Web Design</small> </div>
+              <img src="img/portfolio/IMG_0335.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/IMG_0334.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <small>Web Design</small> </div>
+              <img src="img/portfolio/IMG_0334.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 app">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/IMG_0333.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <small>App Development</small> </div>
+              <img src="img/portfolio/IMG_0333.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 branding">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/IMG_0336.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <small>Branding</small> </div>
+              <img src="img/portfolio/IMG_0336.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 branding app">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/IMG_0338.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <small>App Development, Branding</small> </div>
+              <img src="img/portfolio/IMG_0338.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/IMG_0329.jpg" title="Project description" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Project Title</h4>
+                <small>Web Design</small> </div>
+              <img src="img/portfolio/IMG_0329.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  <div id="contact" class="text-center">
+  <div class="overlay">
+    <div class="container">
+     
+        <div class="social">
+          <ul>
+            <li><a href="https://web.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="container text-center">
+    <div class="fnav">
+      <p>Copyright &copy; Jeffery_Ruth</p>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.js"></script> 
+<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script> 
+<script type="text/javascript" src="js/jquery.isotope.js"></script> 
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="js/contact_me.js"></script> 
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
